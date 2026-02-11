73f3ab59ed339ddc588633f6e5dffca04b3d1f87
Naufaldi Aqil Falah <88096094+naufaldi001@users.noreply.github.com>
2026-02-11 16:48:56 +0700

    Update predict.py

--- Sentiment App/predict.py
+++ Sentiment App/predict.py
@@ -1,10 +1,11 @@
 import joblib
 from preprocess import clean_text
 
-model = joblib.load("naive_bayes_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
+model = joblib.load("/Sentiment App/naive_bayes_model.pkl")
+vectorizer = joblib.load("/Sentiment App/tfidf_vectorizer.pkl")
 
 def predict_sentiment(text):
     clean = clean_text(text)
     vec = vectorizer.transform([clean])
     return model.predict(vec)[0]
+
